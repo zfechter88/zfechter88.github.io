d3a8ef9e63d97e602d1f822cd6c21ebed633c5da
zfechter88 <99442701+zfechter88@users.noreply.github.com>
2022-02-13 18:11:25 -0500

    Update like_button.js

--- like_button.js
+++ like_button.js
@@ -3,18 +3,11 @@
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
     return (
-      <button onClick={() => this.setState({ liked: true }) }>
-        Like
-      </button>
+      <p>holdingp pattern</p>
     );
   }
 }
